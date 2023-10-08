8b176160835a72a818ec14be4f64bba38cf41217
Aelcio Jozias <aelciojoziasputzel18@gmail.com>
2023-10-08 17:52:25 -0300

    [read-all-lines] using bufferedReader to read all lines
    In each line read, was add too the row number.
    I too change the type of object FileInputStream and InputStreamReader to the most super generics type

--- src/br/com/jozias/java/io/teste/TesteLeitura.java
+++ src/br/com/jozias/java/io/teste/TesteLeitura.java
@@ -3,7 +3,9 @@ package br.com.jozias.java.io.teste;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.Reader;
 
 public class TesteLeitura {
 
@@ -12,17 +14,17 @@ public class TesteLeitura {
 		// rapare que quando chamamos o arquivo "lorem.txt", o java vai para a raiz do projeto, seguindo desse ponto como caminho relativo
 		
 		// Fluxo de entrada com um arquivo
-		FileInputStream fileInputStream = new FileInputStream("lorem.txt"); // criando o fluxo completo com arquivo, mas binário. // read() -> bits e bytes
-		InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream); // melhorando os dados binários para caracteres // read() -> caracteres(char)
+		InputStream fileInputStream = new FileInputStream("lorem.txt"); // criando o fluxo completo com arquivo, mas binário. // read() -> bits e bytes
+		Reader inputStreamReader = new InputStreamReader(fileInputStream); // melhorando os dados binários para caracteres // read() -> caracteres(char)
 		BufferedReader bufferedReader = new BufferedReader(inputStreamReader); // esse cara vai pegar o reader e já devolver um linha inteira char -> linha. ou seja, vai pegar os chars e montar uma linha completa.
 		
-		String linha = bufferedReader.readLine();
-		
-		System.out.println(linha);
-		
+		String linha;
+		int rowNumber = 1;
+		while((linha = bufferedReader.readLine()) != null) {
+			System.out.println(rowNumber + " " + linha);
+			rowNumber++;
+		}
 		bufferedReader.close();
-		
-
 	}
 
 }
