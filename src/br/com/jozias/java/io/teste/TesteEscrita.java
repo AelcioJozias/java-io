da02000a69c4a1973a68906f346a6a10398d2e69
Aelcio Jozias <aelciojoziasputzel18@gmail.com>
2023-10-08 18:14:37 -0300

    [criando-e -endo-um-arquivo]

---
+++ src/br/com/jozias/java/io/teste/TesteEscrita.java
@@ -0,0 +1,26 @@
+package br.com.jozias.java.io.teste;
+
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+
+public class TesteEscrita {
+
+	public static void main(String[] args) throws IOException {
+			
+		OutputStream outputStream = new FileOutputStream("lorem2.txt", true); 
+		Writer writer = new OutputStreamWriter(outputStream); 
+		BufferedWriter bufferedWriter = new BufferedWriter(writer);
+		
+		bufferedWriter.write("Testando escrita de arquivo");
+		bufferedWriter.newLine();
+		bufferedWriter.newLine();
+		bufferedWriter.write("Após duas linhas");
+		bufferedWriter.close();
+		
+	}
+
+}
